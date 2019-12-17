1d96aefad7ca93eaa99a8eb622ad371a24a1dcfb
Jacobo Martinez <jacobo.martinez@mediamonks.com>
2019-12-17 19:05:50 -0300

    Fix webpack file-loader plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,34 +1,35 @@
-const Webpack = require('webpack');
+const Webpack = require("webpack");
 module.exports = {
   entry: {
-    vendor: './src/vendor.js',
-    main: './src/index.js'
+    vendor: "./src/vendor.js",
+    main: "./src/index.js"
   },
   module: {
     rules: [
       {
         test: /\.html$/,
-        use: ['html-loader']
+        use: ["html-loader"]
       },
       {
         test: /\.(jpg|jpeg|png|gif|svg|pdf)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[hash].[ext]',
-              outputPath: 'assets'
-            },
-          },
-        ],
-      },
-    ],
+              name: "[name].[hash].[ext]",
+              outputPath: "assets",
+              esModule: false
+            }
+          }
+        ]
+      }
+    ]
   },
   // This config allows to use jQuery $ sign
   plugins: [
     new Webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery'
+      $: "jquery",
+      jQuery: "jquery"
     })
   ]
-}
+};
