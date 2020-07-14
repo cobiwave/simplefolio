aa63ad0fd74bbbc8921ae3f45e3a9edd6880ea66
vikrant karawande <vikrant@mail.mdbrd.in>
2020-07-14 11:02:35 +0530

    updated package.json, updated webpack.dev.js to remove merge is not function error

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const common = require('./webpack.common');
-const merge = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const path = require('path');
 
 module.exports = merge(common, {
