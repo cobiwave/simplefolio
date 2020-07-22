8d06ebc65af513d7bba51e69bb585cc4c0137389
Ephraim Atta-Duncan <[ephraimduncan68@gmail.com]>
2020-07-22 18:04:30 +0000

    Add font-awesome icons

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,8 +1,18 @@
 const theme = () => {
 	const btn = document.querySelector('.btn-toggle');
+	const fa = document.querySelector('i');
+	const sun = 'fa-sun-o';
+	const moon = 'fa-moon-o';
 
 	btn.addEventListener('click', function () {
 		document.body.classList.toggle('dark-theme');
+		if (fa.classList.contains(moon)) {
+			fa.classList.remove(moon);
+			fa.classList.add(sun);
+		} else if (fa.classList.contains(sun)) {
+			fa.classList.remove(sun);
+			fa.classList.add(moon);
+		}
 	});
 };
 export default theme;
