a0717a14762a7f40239f55fb42ea7645c63fa41a
Ephraim Atta-Duncan <[ephraimduncan68@gmail.com]>
2020-07-22 16:53:42 +0000

    Add buttons

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 import initTilt from './js/tilt';
 import initSr from './js/sr';
+import theme from './js/theme';
 
 import './style/main.scss';
 
-$('a[href^="#"]').on('click', function(event) {
-  var target = $(this.getAttribute('href'));
-  if (target.length) {
-    event.preventDefault();
-    $('html, body')
-      .stop()
-      .animate(
-        {
-          scrollTop: target.offset().top
-        },
-        1000
-      );
-  }
+$('a[href^="#"]').on('click', function (event) {
+	var target = $(this.getAttribute('href'));
+	if (target.length) {
+		event.preventDefault();
+		$('html, body').stop().animate(
+			{
+				scrollTop: target.offset().top,
+			},
+			1000,
+		);
+	}
 });
 
+const btn = document.querySelector('.btn-toggle');
+btn.addEventListener('click', function () {
+	document.body.classList.toggle('dark-theme');
+	console.log('btn clicked');
+});
+
+theme();
 initSr();
 initTilt();
---
+++ src/js/theme.js
@@ -0,0 +1,2 @@
+const theme = () => {};
+export default theme;
