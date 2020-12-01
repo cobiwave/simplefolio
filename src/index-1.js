ffe4c18571625a972d12c817a6f82823a39153fe
Another Guest <anotherguest@MacBook-Pro-Georgij.local>
2020-12-01 14:28:55 +0300

    Added new file

---
+++ src/index-1.js
@@ -0,0 +1,22 @@
+import initTilt from './js/tilt';
+import initSr from './js/sr';
+
+import './style/main.scss';
+
+$('a[href^="#"]').on('click', function(event) {
+  var target = $(this.getAttribute('href'));
+  if (target.length) {
+    event.preventDefault();
+    $('html, body')
+      .stop()
+      .animate(
+        {
+          scrollTop: target.offset().top
+        },
+        1000
+      );
+  }
+});
+
+initSr();
+initTilt();
