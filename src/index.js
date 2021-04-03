7b6d3511359cd2a61b965dd124474680b5bbbe38
Jacobo Martinez <jacobojavier98@gmail.com>
2021-04-03 15:38:27 -0300

    add function that add PDF resume

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
-import initTilt from './js/tilt';
-import initSr from './js/sr';
+import initTilt from "./js/tilt";
+import initSr from "./js/sr";
+import resume from "./assets/resume.pdf";
 
-import './style/main.scss';
+import "./style/main.scss";
 
-$('a[href^="#"]').on('click', function(event) {
-  var target = $(this.getAttribute('href'));
+$('a[href^="#"]').on("click", function (event) {
+  var target = $(this.getAttribute("href"));
   if (target.length) {
     event.preventDefault();
-    $('html, body')
-      .stop()
-      .animate(
-        {
-          scrollTop: target.offset().top
-        },
-        1000
-      );
+    $("html, body").stop().animate(
+      {
+        scrollTop: target.offset().top,
+      },
+      1000
+    );
   }
 });
 
+function addResume(pdf) {
+  if (!pdf) return;
+
+  const resumeButton = document.querySelector(".cta-btn--resume");
+  resumeButton.setAttribute("href", pdf);
+}
+
 initSr();
 initTilt();
+
+// uncomment this if you want to attach your resume
+addResume(resume);
