48b9147484c83a0fd20c7c7bf990ba70125e69e2
brurya <8462271@gmail.com>
2021-07-19 14:17:11 +0300

    gh-pages-none

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import initTilt from './js/tilt';
 import initSr from './js/sr';
-var ghpages = require('gh-pages');
-
-ghpages.publish('dist', function(err) {});
-
-ghpages.publish(dir, callback);
-// or...
-ghpages.publish(dir, options, callback);
 
 import './style/main.scss';
 
