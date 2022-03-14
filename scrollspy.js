f5650386895246348d2abdd46c43fcb8cbad925d
alissatroiano <alissatroianonyc@gmail.com>
2022-03-14 10:21:24 -0400

    Update scrollspy js for deployment

---
+++ scrollspy.js
@@ -0,0 +1,19 @@
+//Get the button:
+mybutton = document.getElementById("backToTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	mybutton.style.display = "block";
+  } else {
+	mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
